58db5c549f6d4afc04f7d93a1d289274c804b3fa
Logan Croley <logan.croley.18@gmail.com>
2025-04-24 00:35:26 -0400

    init

---
+++ data/cards/card_stats.gd
@@ -0,0 +1,11 @@
+class_name CardStats
+extends Resource
+
+enum Suit {CLUBS, HEARTS, SPADES,DIAMONDS}
+enum Rank {TWO,THREE, FOUR,FIVE, SIX,SEVEN, EIGHT,NINE, TEN, JACK, QUEEN, KING, ACE}
+@export var name: String
+
+@export_category("Data")
+@export var suit: Suit
+@export var rank: Rank
+@export var value: int
