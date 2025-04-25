f295c68fddaad30e6baf6d27f44eb7c202fc6029
Logan Croley <logan.croley.18@gmail.com>
2025-04-25 15:28:58 -0400

    added some debug configs

--- components/card/card.gd
+++ components/card/card.gd
@@ -5,24 +5,26 @@ extends Sprite2D
 @export var suit: Enums.Suit: set = set_suit
 @export var rank: Enums.Rank: set = set_rank
 
-const card_resource = preload("res://resources/card/card_resource.gd")
-var stats: CardResource : set = set_stats
+const card_resource_preload = preload("res://resources/card/card_resource.gd")
+var card_resource: CardResource : set = set_card_resource
 
 func _ready() -> void:
-	stats = card_resource.new(Enums.Rank.find_key(rank), Enums.Suit.find_key(suit))
-
-func set_stats(value: CardResource) -> void:
-	stats = value
+	card_resource = card_resource_preload.new(Enums.Rank.find_key(rank), Enums.Suit.find_key(suit))
+	if get_tree().current_scene != self:
+		remove_child($Camera2D)
+func set_card_resource(value: CardResource) -> void:
+	card_resource = value
 	if value == null:
 		return
 	if not is_node_ready():
 		await ready
-	region_rect.position = Vector2(stats.skin_coordinates) * 65
+	region_rect.position = Vector2(card_resource.skin_coordinates) * 65
+
 #region these are editor methods to make the tool script work
 func set_suit(val: Enums.Suit):
 	suit=val
-	stats = card_resource.new(Enums.Rank.find_key(rank), Enums.Suit.find_key(suit))
+	card_resource = card_resource_preload.new(Enums.Rank.find_key(rank), Enums.Suit.find_key(suit))
 func set_rank(val: Enums.Rank):
 	rank=val
-	stats = card_resource.new(Enums.Rank.find_key(rank), Enums.Suit.find_key(suit))
+	card_resource = card_resource_preload.new(Enums.Rank.find_key(rank), Enums.Suit.find_key(suit))
 #endregion
--- components/deck.gd
+++ components/deck.gd
@@ -1,9 +1,20 @@
 class_name Deck
 extends Node
 
-var deck_resource_preload := preload("res://resources/deck/deck_resource.gd")
+@export var debugging: bool = false
 
+var deck_resource_preload := preload("res://resources/deck/deck_resource.gd")
+const card_prefab: PackedScene = preload("res://components/card/card.tscn")
 var deck_resource: DeckResource
+
 
 func _ready() -> void:
 	deck_resource = deck_resource_preload.new()
+	if get_tree().current_scene == self:
+		for card_res: CardResource in deck_resource.cards:
+			var card: Card = card_prefab.instantiate()
+			add_child(card)
+			card.card_resource = card_res
+			card.global_position = Vector2((card.card_resource.rank * 65 )+ 65, (card.card_resource.suit * 65)+ 65)
+	else:
+		remove_child($Camera2D)
