a611ece188194de7c15025e0d58587f75841252a
Logan Croley <logan.croley.18@gmail.com>
2025-04-30 23:58:41 -0400

    small refactor + added hitting visually

--- components/dealer/dealer.gd
+++ components/dealer/dealer.gd
@@ -9,8 +9,9 @@ const card_prefab: PackedScene = preload("res://components/card/card.tscn")
 @export var deal_positions: Array[Control] = []
 @export var offset_amount = 32
 
-var current_player
-var cards_to_deal: int 
+var current_player: int = 1
+var cards_to_deal: int
+var tween: Tween
 
 func _ready() -> void:
 	assert(deal_positions.size() >= 2, "Need 2 or more deal_positions") # if there is less then two you don't have a game. Need a dealer + player
@@ -33,26 +34,33 @@ func _clean_up_hand() -> void:
 			pos.remove_child(card)
 			card.queue_free()
 
-func deal_card(to_player: Vector2) -> void:
-	pass
+func deal_card() -> void:
+	_spawn_card(current_player)
 
 func deal() -> void:
 	var deal_index = 0
 	for i in range(cards_to_deal):
-		var deal_pos: Control = deal_positions[deal_index % deal_positions.size()]
-		var card_res: CardResource = shoe.draw()
-		var new_card: Card = card_prefab.instantiate()
-		var target_position = deal_pos.global_position + get_offset(deal_pos)
-		deal_pos.add_child(new_card)
-		new_card.global_position = shoe_pos.global_position
-		new_card.card_resource = card_res
-		var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART)
-		tween.tween_property(new_card, "global_position", target_position, 0.55)
+		var new_card = _spawn_card(i)
 		if i == 0:
 			new_card.is_face_down = true
-		deal_index+=1
 		await tween.finished
 
 func get_offset(deal_pos: Control) -> Vector2:
 	var num_children := deal_pos.get_child_count()
 	return Vector2(offset_amount*num_children, -offset_amount*num_children)
+
+func _spawn_card(deal_index: int) -> Card:
+	var deal_pos: Control = deal_positions[deal_index % deal_positions.size()]
+	var card_res: CardResource = shoe.draw()
+	var new_card: Card = card_prefab.instantiate()
+	var target_position = deal_pos.global_position + get_offset(deal_pos)
+	deal_pos.add_child(new_card)
+	new_card.global_position = shoe_pos.global_position
+	new_card.card_resource = card_res
+	tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART)
+	tween.tween_property(new_card, "global_position", target_position, 0.55)
+	return new_card
+
+
+func _on_stand_pressed() -> void:
+	Signals.end_turn.emit() # emit an end turn event
--- globals/signals.gd
+++ globals/signals.gd
@@ -2,3 +2,5 @@ extends Node
 
 signal deal_new_hand
 signal clean_up_hand
+
+signal end_turn
