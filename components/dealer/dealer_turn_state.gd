21609cec307686f84438e741cfe7ee1ec666a93f
Logan Croley <logan.croley.18@gmail.com>
2025-05-06 17:27:11 -0400

    added dealer reveal

--- components/dealer/dealer_turn_state.gd
+++ components/dealer/dealer_turn_state.gd
@@ -1,9 +1,18 @@
 extends GameState
 
 func enter() -> void:
+	_reveal()
 	await get_tree().create_timer(2).timeout
 	hit()
 	transition_requested.emit(self, State.CLEANUP)
 
 func hit()-> void:
 	dealer.deal_card()
+
+func _reveal() -> void:
+	# TODO: add animations for this?
+	for child in dealer.get_children():
+		if child is not Card:
+			continue
+		var card := (child as Card)
+		card.is_face_down = false
