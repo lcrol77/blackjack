2dc1784744b4eeef27e78f22b85ba51a6e39dce8
Logan Croley <logan.croley.18@gmail.com>
2025-05-17 23:30:53 -0400

    added ability to toggle plus and minus buttoons for betting

--- main.gd
+++ main.gd
@@ -6,22 +6,31 @@ extends Control
 @onready var dealer_label: Label = $DealerLabel
 @onready var bet = $Controls/Bet
 @onready var bank_roll = $BankRoll
+@onready var minus: Button = $Controls/BetControls/Minus
+@onready var plus: Button = $Controls/BetControls/Plus
 
 @export var state_machine: StateMachine 
 @export var players: Array[Player]
 
+var plus_active: bool = false
+var minus_active: bool = false
+
 func _ready() -> void:
 	assert(players.size() >= 1, "Need 1 or more players") # if there is less then two you don't have a game. Need a dealer + player
 	state_machine.init(dealer, players)
-	# current_player signals
+	
+	# current_player init
 	active_player.hand_changed.connect(_update_label.bind(player_label, active_player, false))
 	active_player.hand_confirmed.connect(_update_label.bind(player_label, active_player, true))
 	active_player.bank_roll_changed.connect(_update_bank_roll)
-	active_player.bank_roll_changed.emit(active_player.bank_roll)
-	# dealer signals
+	_update_bank_roll(active_player.bank_roll) # Initialize the bank roll to what the player currently has
+	
+	# dealer init
 	dealer.hand_confirmed.connect(_update_label.bind(dealer_label, dealer, true))
 	dealer.hand_changed.connect(_update_label.bind(dealer_label, dealer,false))
 
+	plus.pressed.connect(_on_plus_minus_toggled.bind(plus, minus))
+	minus.pressed.connect(_on_plus_minus_toggled.bind(minus, plus))
 
 func _update_label(label: Label, player: Player, is_standing: bool) -> void:
 	if player.has_card_hidden:
@@ -51,3 +60,7 @@ func _on_hit_pressed() -> void:
 	if dealer.card_being_delt.is_running():
 		return
 	state_machine.hit()
+
+func _on_plus_minus_toggled(this: Button, other: Button) -> void:
+	if other.button_pressed:
+		other.button_pressed = false
