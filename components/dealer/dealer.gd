96a6ac48c28698ecb382367036fa6ef6fa2468ff
Logan Croley <logan.croley.18@gmail.com>
2025-05-29 21:10:41 -0400

    added rotation options but I dont really like it

--- components/dealer/dealer.gd
+++ components/dealer/dealer.gd
@@ -7,6 +7,8 @@ const card_prefab: PackedScene = preload("res://components/card/card.tscn")
 @export var discard_pos: Control
 @export var offset_amount = 32
 @export_group("debug options")
+@export var use_rotation: bool = false
+@export_range(0.0, 20.0) var rotation_range : float = 0.0
 @export var is_debug: bool = false
 @export var debug_cards_to_deal: Array[CardResource]
 var card_being_delt: Tween
@@ -59,6 +61,8 @@ func _spawn_card(player: Player, is_face_down: bool=false, use_debug: bool = fal
 	new_card.global_position = shoe_pos.global_position
 	new_card.card_resource = card_res
 	new_card.is_face_down=is_face_down
+	if use_rotation:
+		new_card.rotate(deg_to_rad(randf_range(-rotation_range,rotation_range))) 
 	card_being_delt = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART)
 	card_being_delt.tween_property(new_card, "global_position", target_position, 0.55)
 	await card_being_delt.finished
